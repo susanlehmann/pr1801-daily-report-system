e824a47274d20084570b905ffe5603d294faf0f6
NHDang95 <2d2l95@gmail.com>
2018-07-30 20:25:25 +0700

    fix search

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,6 +2,7 @@ class Report < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true
+  validates :reported_at, presence: true
   enum status: {pending: 0, approved: 1, rejected: 2}
 
   scope :load_data, -> {
@@ -10,6 +11,14 @@ class Report < ApplicationRecord
 
   scope :same_division, -> {
     joins(:user).where("users.division_id = ?", User.current.division_id)
+  }
+
+  scope :search_date, -> (search_date) {
+    where("reported_at = ?", search_date)
+  }
+
+  scope :reported_between, -> (start_date, end_date) {
+    where("reported_at >= ? AND reported_at <= ?", start_date, end_date)
   }
 
   def correct_user? user
